e73e7d05a557121f4d71b69f3b7ea14fb3d75e35
mfjunior <maurofiorinjunior@gmail.com>
2020-11-08 18:42:25 -0300

    Exercism solutions for Elixir track -> Strain

---
+++ strain/lib/strain.ex
@@ -0,0 +1,34 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  # def keep([head | tail], fun) do
+  #   case fun.(head) do
+  #     true -> [head | keep(tail, fun)]
+  #     false -> keep(tail, fun)
+  #   end
+  # end
+
+  def keep(list, fun) do
+   for x <- list, fun.(x), do: x
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  # def discard(list, fun) do
+  #   keep(list, &(!fun.(&1)))
+  # end
+
+  def discard(list, fun) do
+   for x <- list, !fun.(x), do: x
+  end
+end
