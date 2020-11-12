9ca9edf4bde38b14cc211d6c76a6cdc6228ab361
mfjunior <maurofiorinjunior@gmail.com>
2020-11-12 20:03:18 -0300

    Exercism solutions for Elixir track -> Space Age

---
+++ space-age/lib/space_age.ex
@@ -0,0 +1,35 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @earth_orbit_days 365.25
+  @earth_orbit_seconds 31_557_600
+
+  @earth_orbital_periods %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    period = Map.get(@earth_orbital_periods, planet)
+    seconds / (period * @earth_orbit_seconds)
+  end
+end
